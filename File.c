d326852798220cc9c08ab4c327b8848cf49e403f
benjaminj26 <88529010+benjaminj26@users.noreply.github.com>
2021-08-22 15:13:45 +0530

    Add files via upload

--- File.c
+++ File.c
@@ -6,10 +6,18 @@
 #include<stdio.h>
 #include<string.h>
 
+struct College
+{
+    char name[20];
+    int rno;
+    char branch[50];
+    int yob;
+};
+
 int main(int argc, char *argv[])
 {
-    FILE *ptr;
     char tr[25];
+    FILE *ptr;
     if(strcmp(argv[1],"write")==0)
     {
         ptr=fopen("Life skills.txt","w");
@@ -34,6 +42,28 @@ int main(int argc, char *argv[])
             fgets(tr,18,ptr);
             printf("%s",tr);
         }
+        fclose(ptr);
+    }
+    else if(strcmp(argv[1],"write-struct")==0)
+    {
+        struct College c;
+        strcpy(c.name,"Benjamin Joseph");
+        c.rno=26;
+        strcpy(c.branch,"Computer Science And Engineering");
+        c.yob=2002;
+        ptr=fopen("Trial.bin","wb");
+        fwrite(&c,sizeof(c),1,ptr);
+        fclose(ptr);
+    }
+    else if(strcmp(argv[1],"read-struct")==0)
+    {
+        struct College d;
+        ptr=fopen("Test.bin","rb");
+        fread(&d,sizeof(d),1,ptr);
+        printf("Name: %s\n",d.name);
+        printf("Roll Number: %d\n",d.rno);
+        printf("Branch: %s\n",d.branch);
+        printf("Year of Birth: %d\n",d.yob);
         fclose(ptr);
     }
     return 0;
