29ccf1e57fca33e468886db1d3802a3368acf109
Benjamin Joseph <88529010+benjaminj26@users.noreply.github.com>
2021-10-07 23:29:21 +0530

    Improvements
    Now all the elements of the structure are dynamically allocated!!

--- Dynamic_Structure.c
+++ Dynamic_Structure.c
@@ -12,16 +12,17 @@
 struct emp
 {
     int empid;
-    char name[20];
-    char surname[50];
-    char address[50];
-    char phone_num[12];
-    char job[50];
+    char *name;
+    char *surname;
+    char *address;
+    char *phone_num;
+    char *job;
 };
 
 //Function for reading the details of the employeess
 void emp_input(size_t size, struct emp *employee)
 {
+    size_t len;
     //Reading the employee id of the employee
     INPUT:printf("Input the employee id: ");
     int id,flag=0;
@@ -43,28 +44,38 @@ void emp_input(size_t size, struct emp *employee)
     getchar();
 
     //Reading the name of the employee
+    len=10;
+    employee[size].name = (char*)malloc(len*sizeof(char));
     printf("Input the name of the employee: ");
-    fgets(employee[size].name,20,stdin);
+    getline(&employee[size].name,&len,stdin);
     employee[size].name[strlen(employee[size].name)-1]='\0';
 
     //Reading the surname of the emoloyee
+    len=10;
+    employee[size].surname = (char*)malloc(len*sizeof(char));
     printf("Input the surname of the employee: ");
-    fgets(employee[size].surname,50,stdin);
+    getline(&employee[size].surname,&len,stdin);
     employee[size].surname[strlen(employee[size].surname)-1]='\0';
 
     //Reading the address of the emoloyee
+    len=10;
+    employee[size].address = (char*)malloc(len*sizeof(char));
     printf("Input the address of the employee: ");
-    fgets(employee[size].address,50,stdin);
+    getline(&employee[size].address,&len,stdin);
     employee[size].address[strlen(employee[size].address)-1]='\0';
 
     //Reading the phone number of the emoloyee
+    len=10;
+    employee[size].phone_num = (char*)malloc(len*sizeof(char));
     printf("Input the phone number of the employee: ");
-    fgets(employee[size].phone_num,12,stdin);
+    getline(&employee[size].phone_num,&len,stdin);
     employee[size].phone_num[strlen(employee[size].phone_num)-1]='\0';
 
     //Reading the job of the emoloyee
+    len=10;
+    employee[size].job = (char*)malloc(len*sizeof(char));
     printf("Input the job of the employee: ");
-    fgets(employee[size].job,50,stdin);
+    getline(&employee[size].job,&len,stdin);
     employee[size].job[strlen(employee[size].job)-1]='\0';
 }
 
@@ -99,9 +110,10 @@ void emp_display(size_t size, struct emp *employee)
 //Function for deleting the details of the employee
 void emp_delete(size_t index, size_t size, struct emp *employee)
 {
+    int i;
     if(index < size)
     {
-        for(int i=index; i<size-1; ++i)
+        for(i=index; i<size-1; ++i)
         {
             /*Copying the details of the employee in front of the employee
             to be deleted to the address of the employee to be deleted*/
@@ -112,8 +124,25 @@ void emp_delete(size_t index, size_t size, struct emp *employee)
             strcpy(employee[i].phone_num, employee[i+1].phone_num);
             strcpy(employee[i].job, employee[i+1].job); 
         }
+        free(employee[i].name);
+        free(employee[i].surname);
+        free(employee[i].address);
+        free(employee[i].phone_num);
+        free(employee[i].job);
     }
     printf("\nDeleted Successfully!!!\n");
+}
+
+void free_all(int size, struct emp *employee)
+{
+    for(int i=0;i<size;++i)
+    {
+        free(employee[i].name);
+        free(employee[i].surname);
+        free(employee[i].address);
+        free(employee[i].phone_num);
+        free(employee[i].job);
+    }
 }
 
 int main()
@@ -186,41 +215,47 @@ int main()
                 printf("\nINVALID USER ID!!!\n");
             else
             {
+                size_t len;
                 int edit_choice;
                 char ch;
-                do
+                LOOP:do
                 {
                     printf("\n1.Name\n2.Surname\n3.Address\n4.Phone Number\n5.Job\nInput the detail you want to edit: ");
                     scanf("%d",&edit_choice);
                     getchar();
                     if(edit_choice == 1)
                     {
+                        len=10;
                         printf("Input the new name: ");
-                        fgets(employees[i].name,20,stdin);
+                        getline(&employees[i].name,&len,stdin);
                         employees[i].name[strlen(employees[i].name)-1]='\0';
                     }
                     else if(edit_choice == 2)
                     {
+                        len=10;
                         printf("Input the new surname: ");
-                        fgets(employees[i].surname,50,stdin);
+                        getline(&employees[i].surname,&len,stdin);
                         employees[i].surname[strlen(employees[i].surname)-1]='\0';
                     }
                     else if(edit_choice == 3)
                     {
+                        len=10;
                         printf("Input the new address: ");
-                        fgets(employees[i].address,50,stdin);
+                        getline(&employees[i].address,&len,stdin);
                         employees[i].address[strlen(employees[i].address)-1]='\0';
                     }
                     else if(edit_choice == 4)
                     {
+                        len=10;
                         printf("Input the new phone number: ");
-                        fgets(employees[i].phone_num,12,stdin);
+                        getline(&employees[i].phone_num,&len,stdin);
                         employees[i].phone_num[strlen(employees[i].phone_num)-1]='\0';
                     }
                     else if(edit_choice == 5)
                     {
+                        len=10;
                         printf("Input the new job: ");
-                        fgets(employees[i].job,50,stdin);
+                        getline(&employees[i].job,&len,stdin);
                         employees[i].job[strlen(employees[i].job)-1]='\0';
                     }
                     printf("Do you want to continue?(y/n): ");
@@ -229,10 +264,13 @@ int main()
             }
         }
         else if(option == 5)
+        {
+            free_all(size,employees);
+            free(employees);
             exit(0);
+        }
         else
-            exit(0);
+            goto LOOP;
     }while(1);
-    free(employees);
     return 0;
 }
