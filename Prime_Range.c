a91347aa2d4693a38b04e063c41809072dfb4294
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2021-10-15 23:24:42 +0530

    Prime_Range.c Perf Improved

--- Prime_Range.c
+++ Prime_Range.c
@@ -8,19 +8,29 @@
 int main()
 {
     printf("Input the lower limit: ");
-    int low;scanf("%d",&low);
+    int low;
+    scanf("%d",&low);
     printf("Input the upper limit: ");
-    int upp;scanf("%d",&upp);
-    for(int i=low;i<upp;i+=1)
+    int upp;
+    scanf("%d",&upp);
+    if(low < upp)
     {
-        int flag=0;
-        for(int j=2;j<(i+1)/2;j+=1)
+        for(int i=low;i<upp;i+=1)
         {
-            if(i%j==0)
-                flag+=1;
+            int flag=0;
+            for(int j=2;j<(i+1)/2;j+=1)
+            {
+                if(i%j==0)
+                {
+                    flag+=1;
+                    break;
+                }
+            }
+            if(flag==0 && i!=4)
+                printf("%d\n",i);
         }
-        if(flag==0 && i!=1 && i!=4)
-            printf("%d\n",i);
     }
+    else
+        printf("\nOperation not possible!!\n");
     return 0;
 }
